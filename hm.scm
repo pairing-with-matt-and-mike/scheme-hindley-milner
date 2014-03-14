0feec6df01e085776174f8baec30318a97a88c1f
Michael Williamson <mike@zwobble.org>
2014-03-14 21:34:37 +0000

    Basic unification of types... almost.

--- hm.scm
+++ hm.scm
@@ -2,48 +2,67 @@
 (define (fn? node) (eq? (car node) 'fn))
 (define variable? symbol?)
 (define fn-type? list?)
+(define free-type? number?)
 
-(define (infer-type* env node)
+(define (unify-types first-type second-type)
+  (cond ((equal? first-type second-type)
+         first-type)
+        ((free-type? first-type) second-type)
+        ((free-type? second-type) first-type)
+        ((and (fn-type? first-type) (fn-type? second-type))
+         `(,(unify-types (car first-type) (car second-type))
+           ,(unify-types (cadr first-type) (cadr second-type))))
+        (#t #f)))
+
+(define (infer-type* env free-type-counter node)
   (cond ((number? node) 'Number)
         ((string? node) 'String)
         ((variable? node) (cadr (assoc node env)))
         ((fn? node)
-         (let* ((formal-arg      (cadr node))
-                (formal-arg-type (car formal-arg))
-                (formal-arg-name (cadr formal-arg))
-                (body-env (cons `(,formal-arg-name ,formal-arg-type) env)))
-           `(,(caadr node)
-             ,(infer-type* body-env (caddr node)))))
+         (let* ((formal-arg-name (cadr node))
+                (body-env (cons `(,formal-arg-name ,free-type-counter) env)))
+           `(,free-type-counter
+             ,(infer-type* body-env (+ 1 free-type-counter) (caddr node)))))
         ((apply? node)
-         (let ((callee-type (infer-type* env (cadr node))))
+         (let* ((callee-type* (infer-type* env free-type-counter (cadr node)))
+                (abstract-callee-type `(,free-type-counter ,(+ 1 free-type-counter)))
+                (callee-type (unify-types callee-type* abstract-callee-type)))
            (if (fn-type? callee-type)
-               (if (equal? (infer-type* env (caddr node)) (car callee-type))
-                   (cadr callee-type)
-                   #f)
+               (let* ((formal-arg-type (car callee-type))
+                      (actual-arg-type (infer-type* env (+ 2 free-type-counter) (caddr node)))
+                      (concrete-arg-type (unify-types formal-arg-type actual-arg-type))
+                      (return-type (cadr callee-type)))
+                 (if (free-type? return-type)
+                     concrete-arg-type
+                     return-type))
                #f)))))
 
-(define (infer-type node) (infer-type* '() node))
+(define (infer-type node) (infer-type* '() 0 node))
 
 (define (assert-equal expected actual)
-  (if (not(equal? expected actual))
+  (if (not (equal? expected actual))
       (printf "expected ~s but was ~s\n" expected actual)
-      (print "OK")))
+      (print "OK " expected)))
 
-(assert-equal 'Number (infer-type 1))
+(assert-equal 'Number          (infer-type 1))
 
-(assert-equal 'String (infer-type "blah"))
+(assert-equal 'String          (infer-type "blah"))
 
-(assert-equal '(String Number) (infer-type '(fn (String x) 1)))
+(assert-equal '(0 Number)      (infer-type '(fn x 1)))
 
-(assert-equal 'Number (infer-type '(apply (fn (String x) 1) "asdf")))
+(assert-equal '(0 (1 Number))  (infer-type '(fn x (fn y 1))))
 
-(assert-equal #f (infer-type '(apply (fn (String x) 1) 1)))
+(assert-equal 'Number          (infer-type '(apply (fn x 1) "asdf")))
 
-(assert-equal #f (infer-type '(apply 1 1)))
+(assert-equal #f               (infer-type '(apply 1 1)))
 
-(assert-equal '(Number Number) (infer-type '(fn (Number x) x)))
+(assert-equal '(0 0)           (infer-type '(fn x x)))
 
-(assert-equal 'Number (infer-type
-                       '(apply
-                         (fn ((Number Number) f) (apply f 1))
-                         (fn (Number x) x))))
+(assert-equal 'Number          (infer-type '(apply (fn x x) 1)))
+
+(assert-equal '((Number 0) 0)  (infer-type '(fn f (apply f 1))))
+
+(assert-equal 'Number          (infer-type
+                                '(apply
+                                  (fn f (apply f 1))
+                                  (fn x x))))
